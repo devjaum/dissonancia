9282bc2b7ef23eaae3349a5677a945a3c1a9b1b6
João Carlos <devjaum@gmail.com>
2025-11-19 19:34:19 -0300

    Adicionando talentos

--- src/createPerso/create.ts
+++ src/createPerso/create.ts
@@ -34,7 +34,25 @@ export class CreatePerso {
       {name: "Treinamento em combate", description: "Você recebe +1 em testes de ataque", peso: 2},
       {name: "Vigoroso", description: "Você recebe +10 pontos de vida", peso: 2},
       {name: "Conhecimento de Batalha", description: "Uma vez por combate, você pode gastar 1 PE para identificar as resistências e vulnerabilidades de um inimigo", peso: 2},
-      
+      {name: "Furtividade", description: "Você é especialmente bom em nao ser percebido pelos inimigos, +2 em testes de Furtividade", peso: 2},
+      {name: "Regenerar", description: "Regenera pontos de vida no meio do combate (+1 a cada 2 turnos)", peso: 8},
+      {name: "Conhecimento arcano", description: "Você possui vasto conhecimento sobre magias e rituais, podendo reconhece-los +2 em testes de inteligência", peso: 2},
+      {name: "Polivalente", description: "Escolha um atributo. Você recebe +1 em todos os testes relacionados a esse atributo e pode escolher uma perícia associada para receber +2 nela", peso: 7},
+      {name: "Prodígio", description: "Você recebe +1 em todos os atributos", peso: 7},
+      {name: "Dualidade", description: "Você pode ocultar sua verdadeira especialidade em combate. Escolha dois atributos. Você pode trocar os valores desses atributos uma vez por combate como ação bônus", peso: 5},
+      {name: "Fortaleza Viva", description: "Você não pode se mover por 2 turnos, mas recebe +5 de defesa e regenera 2 PV por turno", peso: 8},
+      {name: "Execução Rápida", description: "Se derrotar um inimigo com um ataque corpo a corpo, pode usar sua ação bônus para realizar outro ataque corpo a corpo contra outro inimigo adjacente", peso: 4},
+      {name: "Guardião Protetor", description: "Quando um aliado em alcance curto for atacado, você pode usar sua reação para interpor-se e receber o ataque no lugar dele", peso: 4},
+      {name: "Oportunista", description: "Quando um aliado próximo ataca um alvo que você possa ver, você pode usar sua ação bônus para realizar um ataque corpo a corpo contra esse alvo", peso: 4},
+      {name: "Recarga Interna", description: "Recupera +1 de shinsu a cada 2 turnos", peso: 6},
+      {name: "Forma Final", description: "Ao cair abaixo de 15% de vida, você pode ativar sua forma final como ação. Durante 3 turnos, você recebe +5 em todos os atributos e regenera 5 PV por turno. Só pode ser usado uma vez por combate.", peso: 10},
+      {name: "Vínculo Vital", description: "Escolha um aliado no início do combate. Enquanto estiver consciente, você recebe todo o dano que ele receberia", peso: 8},
+      {name: "Inevitável", description: "Escolha um inimigo no início do combate. Você recebe +2 em todos os testes contra ele até que um de vocês caia", peso: 6},
+      {name: "Última Chance", description: "Ao cair a 0 PV, você pode realizar uma última ação com sucesso garantido. Se eliminar o alvo dessa ação em até 2 turnos, você revive com 1d6 PV. Só pode ser usado uma vez por combate.", peso: 10},  
+      {name: "Cronometro", description: "O som de um relógio está constantemente em sua mente, permitindo se adaptar ao tempo de forma única. Você pode gastar 3 PE para agir novamente no final do turno atual (Ação bônus).", peso: 8},
+      {name: "Olhar da Morte", description: "Uma vez por combate, você pode marcar um inimigo com sua sentença. Durante 3 turnos, seus ataques contra ele ignoram defesa, causam +1d6 de dano e não podem ser curados. Se o inimigo morrer nesse período, você recupera todo seu Shinsu.", peso: 10},
+      {name: "Marca da Redenção", description: "Uma vez por combate, você pode marcar até 2 inimigos em alcance médio. Durante 2 turnos, qualquer dano causado a ele recupera toda a vida perdida do atacante. A marca desaparece se o inimigo morrer.", peso: 10},
+  
     ]
     listaTalent = signal([""]);
     forca = signal(2);
@@ -45,9 +63,14 @@ export class CreatePerso {
     carisma = signal(2);
     pointsAtt = signal(10);
     pointsT = signal(10);
-    vida = computed(() => 5 + this.constituicao() * 10 + this.forca() * 5);
+    vidaExtra = 0;
+    vida = computed(() => 5 + this.constituicao() * 10 + this.forca() * 5 + this.vidaExtra);
     shinsu = computed(() => 4 + this.inteligencia() * 12 + this.sabedoria() * 6);
     energia = computed(() => 1 + this.destreza() * 3 + this.carisma() * 2);
+
+    ngOnInit(){
+      this.talent.sort((a, b) => a.peso - b.peso);
+    }
 
     showDescription(talent: any) {
       let description = document.getElementById(talent.name+talent.peso);
@@ -86,6 +109,20 @@ export class CreatePerso {
       //Mudar a cor do talento selecionado
       this.mudarCorTalent(talent);
       
+      //Verificar quais talentos adiciona pontos aos atributos (força...)
+      if(talent.name === "Prodígio"){
+        this.forca.set(this.forca() + 1);
+        this.destreza.set(this.destreza() + 1);
+        this.constituicao.set(this.constituicao() + 1);
+        this.inteligencia.set(this.inteligencia() + 1);
+        this.sabedoria.set(this.sabedoria() + 1);
+        this.carisma.set(this.carisma() + 1);
+      }
+      if(talent.name === "Vigoroso"){
+        //Aumentar a vida em 10
+        this.vidaExtra = 10;
+      }
+
       return;
 
     }
@@ -96,6 +133,20 @@ export class CreatePerso {
       
       //Remover a cor do talento
       this.removerCorTalent(talent);
+
+      //Verificar se o talento tirado altera os pontos dos atributos
+      if(talent.name === "Prodígio"){
+        this.forca.set(this.forca() - 1);
+        this.destreza.set(this.destreza() - 1);
+        this.constituicao.set(this.constituicao() - 1);
+        this.inteligencia.set(this.inteligencia() - 1);
+        this.sabedoria.set(this.sabedoria() - 1);
+        this.carisma.set(this.carisma() - 1);
+      }
+      if(talent.name === "Vigoroso"){
+        this.vidaExtra = 0;
+      }
+      
       return;
     }
 
@@ -216,6 +267,25 @@ export class CreatePerso {
     }
 
     reset(){
+      let aux = 0;
+      //Verificar se possui talento que altera o atributo
+      this.listaTalent().forEach(talentName => {
+        let talent = this.getTalent(talentName);
+        if(talent){
+          if(talent.name === "Prodígio"){
+            this.forca.set(3);
+            this.destreza.set(3);
+            this.constituicao.set(3);
+            this.inteligencia.set(3);
+            this.sabedoria.set(3);
+            this.carisma.set(3);
+            this.pointsAtt.set(10);
+            aux = 1;
+          }
+        }
+      });
+      
+      if(aux === 0){
         this.forca.set(2);
         this.destreza.set(2);
         this.constituicao.set(2);
@@ -223,6 +293,8 @@ export class CreatePerso {
         this.sabedoria.set(2);
         this.carisma.set(2);
         this.pointsAtt.set(10);
+      }
+      return;
     }
     constructor() {}
 }
