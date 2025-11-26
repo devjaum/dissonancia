8daefc00152d8ba00f24bb3ba1fee15f78c3fd9c
Joao Carlos <devjaum@gmail.com>
2025-11-26 14:21:33 -0300

    Melhorias no frontEnd

--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -1,13 +1,18 @@
 import { Injectable, inject } from '@angular/core';
 import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
     private auth = inject(Auth);
+    private firestore = inject(Firestore);
 
     login(email: string, password: string) {
         return signInWithEmailAndPassword(this.auth, email, password);
     }
+
+   
 }
